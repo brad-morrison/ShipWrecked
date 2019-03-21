0e784d9f8a2858bb22b3aa4a35254b71c1e369b2
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-03-21 14:07:07 +0000

    added costumes

--- Assets/Scripts/Outfits.cs
+++ Assets/Scripts/Outfits.cs
@@ -8,6 +8,9 @@ public class Outfits : MonoBehaviour
     
     public GameObject pirate, pirateShoot, pirateKnife;
     public GameObject skeleton, skeletonShoot, skeletonKnife;
+    public GameObject tinman, tinmanShoot, tinmanKnife;
+    public GameObject zombie, zombieShoot, zombieKnife;
+    public GameObject wizard, wizardKnife, wizardShoot;
 
     int outfit;
     
@@ -16,7 +19,6 @@ public class Outfits : MonoBehaviour
     {
         GM = GameObject.Find("scripts").GetComponent<GameManager>();
         outfit = PlayerPrefs.GetInt("outfit", 0); //replace this with playerPrefs
-
         activateOutfit();
     }
 
@@ -36,6 +38,27 @@ public class Outfits : MonoBehaviour
                 GM.player = skeleton;
                 GM.knifePrefab = skeletonKnife;
                 GM.shootPrefab = skeletonShoot;
+                break;
+
+            case 2:
+                tinman.SetActive(true);
+                GM.player = tinman;
+                GM.knifePrefab = tinmanKnife;
+                GM.shootPrefab = tinmanShoot;
+                break;
+
+            case 3:
+                zombie.SetActive(true);
+                GM.player = zombie;
+                GM.knifePrefab = zombieKnife;
+                GM.shootPrefab = zombieShoot;
+                break;
+
+            case 4:
+                wizard.SetActive(true);
+                GM.player = wizard;
+                GM.knifePrefab = wizardKnife;
+                GM.shootPrefab = wizardShoot;
                 break;
         }
     }
